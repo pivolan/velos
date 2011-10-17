850e735c62c8ca8a694f863f06e8345c73e4ba0d
pivolan@gmail.com <pivo@.(none)>
2011-10-18 03:20:20 +0400

    layout in haml

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,13 +1,14 @@
 class IndexController < ApplicationController
 
-	def index
-		@user = User.find(1)
+  def index
+    @user = User.find(1)
 
-		@photos = Photo.all
-		#Photo.create(:title => 'sdfsd')
-		@user.photos.push(@photos)
-	end
-	def vasya
+    @photos = Photo.all
+    #Photo.create(:title => 'sdfsd')
+    @user.photos.push(@photos)
+  end
 
-	end
+  def vasya
+
+  end
 end
