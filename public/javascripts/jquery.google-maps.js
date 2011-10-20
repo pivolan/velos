858f397f5accb124ea8fa724a45c38e9ef5fa5f2
dachiko <dachiko007@gmail.com>
2011-10-21 01:23:57 +0400

    переход на стандартный "шаблонизатор" руби

---
+++ public/javascripts/jquery.google-maps.js
@@ -0,0 +1,19 @@
+/**
+ * Created by JetBrains RubyMine.
+ * User: Dachiko
+ * Date: 21.10.11
+ * Time: 0:39
+ * To change this template use File | Settings | File Templates.
+ */
+
+function init()
+{
+	var myLatlng = new google.maps.LatLng(-34.397, 150.644);
+	var myOptions = {
+			zoom: 8,
+			center: myLatlng,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+
+	var map = new google.maps.Map(document.getElementById("track-map-create"), myOptions);
+}
