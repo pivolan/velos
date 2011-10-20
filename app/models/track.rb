e85ea21b2c3dbc98204d1a28a97204db7d74ed1e
pivo <pivolan@gmail.com>
2011-10-20 19:21:31 +0400

    пример геолокации

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,8 +1,8 @@
 class Track
   include MongoMapper::Document
-  #plugin GeoSpatial
+  plugin GeoSpatial
 
 	key :name, String
   belongs_to :user
-	#geo_key :coords, Array
-end 
+	geo_key :coords, Array
+end
